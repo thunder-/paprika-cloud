d305de118431c8cb4b109a13d08650b0f3e4361a
Gert van der Spoel <gert.van.der.spoel@oxyma.nl>
2019-07-05 13:17:46 +0200

    changed db to utf8mb4 where the max varchar length is 191 for unique indexed columns

--- paprika_cloud/paprika-cloud-db/mysql/create/paprika_cloud/ddl/tab/queues.sql
+++ paprika_cloud/paprika-cloud-db/mysql/create/paprika_cloud/ddl/tab/queues.sql
@@ -2,7 +2,7 @@ CREATE TABLE queues
 (
   id           int(11) NOT NULL AUTO_INCREMENT primary key,
   hashcode     varchar(255),
-  name         varchar(255) UNIQUE NOT NULL,
+  name         varchar(191) UNIQUE NOT NULL,
   tablename    varchar(255) NOT NULL,
   active       int(1) NOT NULL DEFAULT 1,
   created_at   datetime,
